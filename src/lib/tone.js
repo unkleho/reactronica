07f734324c5a044ff92bd568520a7156072857b2
Kaho Cheung <kaho@elefanttraks.com>
2019-07-28 23:47:54 +1000

    Merge tone 13.4.9. Update website to use tone as peerDep

--- src/lib/tone.js
+++ src/lib/tone.js
@@ -1,11 +1,12 @@
-// import Tone from 'tone';
+import Tone from 'tone';
 
-let tone;
+// let tone;
 
-if (typeof window !== 'undefined') {
-  tone = require('Tone');
-} else {
-  tone = null;
-}
+// if (typeof window !== 'undefined') {
+//   tone = require('Tone');
+// } else {
+//   tone = null;
+// }
 
-export default tone;
+export default Tone;
+// export default tone;
