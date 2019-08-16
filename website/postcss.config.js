13cc33e36bf1181182a24e5a3212c526ac35e4aa
Kaho Cheung <kaho@elefanttraks.com>
2019-08-16 23:49:21 +1000

    Add postcss-import and postcss-extend-rule

--- website/postcss.config.js
+++ website/postcss.config.js
@@ -1,10 +1,12 @@
 module.exports = {
   plugins: [
+    require('postcss-import')(),
     require('postcss-preset-env')({
       stage: 3,
       features: {
         'nesting-rules': true,
       },
     }),
+    require('postcss-extend-rule')(),
   ],
 };
