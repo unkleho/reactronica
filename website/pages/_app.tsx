518964be9831a01827af8561d2c85f3c663a9603
Kaho Cheung <kaho@elefanttraks.com>
2019-09-04 21:55:43 +1000

    Update home page

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import NextApp from 'next/app';
+import { MDXProvider } from '@mdx-js/react';
 
 import DocApp from '../components/DocApp';
 import DAWApp from '../components/DAWApp';
+import CodeBlock from '../components/CodeBlock';
 
 class MyApp extends NextApp {
   render() {
@@ -16,10 +18,18 @@ class MyApp extends NextApp {
       AppComponent = DocApp;
     }
 
+    const components = {
+      pre: (props) => <div {...props} />,
+      // code: (props) => <pre style={{ color: 'tomato' }} {...props} />,
+      code: CodeBlock,
+    };
+
     return (
-      <AppComponent>
-        <Component {...pageProps} />
-      </AppComponent>
+      <MDXProvider components={components}>
+        <AppComponent>
+          <Component {...pageProps} />
+        </AppComponent>
+      </MDXProvider>
     );
   }
 }
