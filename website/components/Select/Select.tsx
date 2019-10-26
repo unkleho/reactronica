7e5863d005857de4710aac686143ac24328603bf
Kaho Cheung <kaho@elefanttraks.com>
2019-10-26 22:05:12 +1100

    Fix react-select focus issue

--- website/components/Select/Select.tsx
+++ website/components/Select/Select.tsx
@@ -32,4 +32,16 @@ const SelectComponent: React.FunctionComponent<Props> = ({
   );
 };
 
-export default SelectComponent;
+// Need to compare options array by stringifying first, as array comparisons
+// always equal false, even when the values are the same.
+const areEqual = (prevProps, nextProps) => {
+  return (
+    prevProps.value === nextProps.value &&
+    JSON.stringify(prevProps.options) === JSON.stringify(nextProps.options) &&
+    prevProps.className === nextProps.className
+  );
+};
+
+// Memo is required to prevent React Select from re-rendering
+// and losing focus due to onStepPlay callback while Song is playing
+export default React.memo(SelectComponent, areEqual);
