156b306e97ba6b033ca776a8e85ff9f2c9711cde
Kaho Cheung <kaho@elefanttraks.com>
2020-01-02 23:26:58 +1100

    Add tests for Track, Instrument and Effect components

--- src/__mocks__/tone.js
+++ src/__mocks__/tone.js
@@ -1,51 +1,26 @@
-// import Tone from 'tone';
+/**
+ * Tone JS Mock
+ *
+ * NOTE: Tone's sub-module classes are rebuilt with injected mock functions so
+ * Jest can spy on them. This isn't the best option, but I was unable to mock
+ * Tone's classes using mockImplementation as it is difficult to access them
+ * directly.
+ */
 
-// WIP Mock of Tone
-// NOTE: Not sure if this is best approach
+// ----------------------------------------------------------------------------
+// Tone.Master
+// ----------------------------------------------------------------------------
 
-// import Tone from 'tone';
+const Master = {
+  volume: 0,
+  mute: false,
+  chain: jest.fn(),
+  dispose: jest.fn(),
+};
 
-class PanVol {
-  constructor(pan, volume) {
-    this.volume = {
-      value: volume,
-    };
-
-    this.pan = {
-      value: pan,
-    };
-  }
-}
-
-export const mockFunction = jest.fn();
-
-class PolySynth {
-  constructor(options) {
-    // console.log(options);
-
-    this.cleanup = jest.fn();
-    this.dispose = jest.fn();
-    this.chain = jest.fn();
-    this.disconnect = jest.fn();
-  }
-}
-
-export const sequenceMock = jest.fn();
-
-class Sequence {
-  constructor(callback, steps) {
-    /**
-     * Unable to mock constructor calls using mockImplementation as it is difficult to access Tone's submodule classes directly.
-     * Need to manually spy on the constructor's arguments with injected mock functions.
-     */
-    sequenceMock(steps);
-    // console.log(steps[0]);
-
-    this.start = mockFunction;
-    this.removeAll = jest.fn();
-    this.dispose = jest.fn();
-  }
-}
+// ----------------------------------------------------------------------------
+// Tone.Transport
+// ----------------------------------------------------------------------------
 
 const Transport = {
   bpm: {
@@ -55,19 +30,108 @@ const Transport = {
   stop: jest.fn(),
 };
 
+// ----------------------------------------------------------------------------
+// Tone.PanVol
+// ----------------------------------------------------------------------------
+
+export const mockPanVolConstructor = jest.fn();
+export const mockPanVolVolume = jest.fn();
+export const mockPanVolPan = jest.fn();
+
+class PanVol {
+  constructor(pan, volume) {
+    mockPanVolConstructor(pan, volume);
+
+    this.volume = {
+      value: volume,
+    };
+
+    this.pan = {
+      value: pan,
+    };
+
+    mockPanVolVolume(this.volume.value);
+    mockPanVolPan(this.pan.value);
+  }
+}
+
+// ----------------------------------------------------------------------------
+// Tone.PolySynth
+// ----------------------------------------------------------------------------
+
+export const mockPolySynthConstructor = jest.fn();
+export const mockPolySynthDispose = jest.fn();
+export const mockPolySynthChain = jest.fn();
+
+class PolySynth {
+  constructor(polyphony, voice, voiceArgs) {
+    mockPolySynthConstructor(polyphony, voice, voiceArgs);
+
+    this.dispose = mockPolySynthDispose;
+    this.chain = mockPolySynthChain;
+    this.disconnect = jest.fn();
+  }
+}
+
+// ----------------------------------------------------------------------------
+// Tone.Sampler
+// ----------------------------------------------------------------------------
+
+export const mockSamplerConstructor = jest.fn();
+export const mockSamplerDispose = jest.fn();
+
+class Sampler {
+  constructor(samples) {
+    mockSamplerConstructor(samples);
+
+    this.dispose = mockSamplerDispose;
+    this.chain = jest.fn();
+    this.disconnect = jest.fn();
+  }
+}
+
+// ----------------------------------------------------------------------------
+// Tone.AutoFilter
+// ----------------------------------------------------------------------------
+
+export const mockAutoFilterConstructor = jest.fn();
+// export const mockAutoFilterDispose = jest.fn();
+
+class AutoFilter {
+  constructor() {
+    mockAutoFilterConstructor();
+
+    // this.dispose = mockAutoFilterDispose;
+    // this.chain = jest.fn();
+    // this.disconnect = jest.fn();
+  }
+}
+
+// ----------------------------------------------------------------------------
+// Tone.Sequence
+// ----------------------------------------------------------------------------
+
+export const mockSequenceConstructor = jest.fn();
+
+class Sequence {
+  constructor(callback, steps) {
+    mockSequenceConstructor(steps);
+
+    this.start = jest.fn();
+    this.stop = jest.fn();
+    this.add = jest.fn();
+    this.removeAll = jest.fn();
+    this.dispose = jest.fn();
+  }
+}
+
 const MockTone = {
+  Master,
+  Transport,
   PanVol,
   PolySynth,
-  // This doesn't work unfortunately
-  // PolySynth: jest.fn().mockImplementation(() => {
-  //   return {
-  //     cleanup: mockFunction,
-  //     dispose: mockFunction,
-  //     chain: mockFunction,
-  //     disconnect: mockFunction,
-  //   };
-  // }),
-  Transport,
+  Sampler,
+  AutoFilter,
   Sequence,
 };
 
