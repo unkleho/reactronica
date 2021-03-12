c54ecb5581ffd3bb1f8ed45aafc68b9e2ecc0a0d
Kaho Cheung <kaho@elefanttraks.com>
2021-03-12 19:15:32 +1100

    Update mdx and react

--- website/pages/steps-editor.tsx
+++ website/pages/steps-editor.tsx
@@ -1,22 +1,27 @@
 import React from 'react';
-import { Song, Track, Instrument } from 'reactronica';
+import { Song, Track, Instrument, StepNoteType } from 'reactronica';
 
 import DAWStepsEditor from '../components/DAWStepsEditor';
+import SamplerInstrument from '../components/SamplerInstrument';
 import { StepIndexContext } from '../lib/contexts/StepIndexContext';
 
 const Page = () => {
   const [isPlaying, setIsPlaying] = React.useState(false);
   const [stepIndex, setStepIndex] = React.useState(0);
   const [notes, setNotes] = React.useState(null);
-  const [steps, setSteps] = React.useState([
-    { name: 'C3' },
-    { name: 'C3' },
-    { name: 'C3' },
-    { name: 'C3' },
-    { name: 'C3' },
-    { name: 'C3' },
-    { name: 'C3' },
-    { name: 'C3' },
+  const [steps, setSteps] = React.useState<StepNoteType[][]>([
+    [
+      { name: 'D3', duration: 8, velocity: 1 },
+      { name: 'C3', duration: 8, velocity: 0 },
+      { name: 'E3', duration: 8 },
+    ],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
   ]);
 
   return (
@@ -24,13 +29,13 @@ const Page = () => {
       <StepIndexContext.Provider value={{ currentStepIndex: 0 }}>
         <DAWStepsEditor
           clipId="Melody"
-          // @ts-ignore
           steps={steps}
           startNote="C3"
           endNote="B3"
           // onStepsChange={(steps, noteName, index) => {
           //   setSteps(steps);
           // }}
+
           // currentStepIndex={stepIndex}
         />
       </StepIndexContext.Provider>
@@ -45,7 +50,7 @@ const Page = () => {
           ]);
         }}
         onMouseUp={() => {
-          // setNotes(null);
+          setNotes(null);
         }}
       >
         Play Note
@@ -58,24 +63,28 @@ const Page = () => {
       >
         {isPlaying ? 'Stop' : 'Play'}
       </button>
-      <Song isPlaying={isPlaying}>
+      <Song isPlaying={isPlaying} bpm={70}>
         <Track
           steps={steps}
           onStepPlay={(_, index) => {
             setStepIndex(index);
           }}
         >
-          <Instrument
+          <SamplerInstrument notes={notes} />
+          {/* <Instrument
             type="sampler"
             notes={notes}
             samples={{
-              C3: '/audio/ukulele/Fluke_Uke_060.wav',
-              D3: '/audio/ukulele/Fluke_Uke_062.wav',
+              C3: '/audio/DBC_70_lofi_melodic_kalimba_action_Cm.wav',
+              D3: '/audio/DECAP_140_drum_loop_baptized_bouncy_rimshot.wav',
+              E3: '/audio/OS_NC_140_Cm_Octagon_Guitar.wav',
+              // C3: '/audio/ukulele/Fluke_Uke_060.wav',
+              // D3: '/audio/ukulele/Fluke_Uke_062.wav',
             }}
             // options={{
             //   release: 3,
             // }}
-          />
+          /> */}
         </Track>
       </Song>
     </div>
